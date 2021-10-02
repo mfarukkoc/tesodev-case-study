8d7d04600e01d4722bd91331d535050a630f0c64
Mehmet Faruk Koç <mfarukkoc@gmail.com>
2021-10-02 13:58:29 +0300

    feat(app): Add page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ResultPage from "./pages/ResultPage";
+
 function App() {
-  return <div>Homepage</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/result">
+          <ResultPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
